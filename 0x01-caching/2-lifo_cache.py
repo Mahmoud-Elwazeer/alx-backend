150e77fc39f54081be95096de9899ee29e7812f1
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-31 21:54:37 +0200

    try to fix lifo alg

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,15 +10,14 @@ class LIFOCache(BaseCaching):
         """put key, value pair in dict"""
         if key is None or item is None:
             return
-        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
-            if (key in self.cache_data):
+        if (key in self.cache_data):
                 del self.cache_data[key]
                 self.cache_data[key] = item
                 return
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
             rm_key = list(self.cache_data.keys())[3]
             del self.cache_data[rm_key]
             print("DISCARD: {}".format(rm_key))
-        
         self.cache_data[key] = item
     
     def get(self, key):
