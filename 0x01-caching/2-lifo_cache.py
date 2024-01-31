74750cca7e00cc3f8a0e9be9d4ec611c8d9ef664
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-31 21:46:45 +0200

    LIFO cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -3,28 +3,31 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """FIFO Cashe"""
+class LIFOCache(BaseCaching):
+    """LIFO Cache nherits from BaseCaching"""
 
     def put(self, key, item):
         """put key, value pair in dict"""
         if key is None or item is None:
-            pass
+            return
         if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
             if (key in self.cache_data) and self.cache_data[key] == item:
                 return
             elif (key in self.cache_data):
+                del self.cache_data[key]
                 self.cache_data[key] = item
                 return
-            rm_key = list(self.cache_data.keys())[0]
+            rm_key = list(self.cache_data.keys())[3]
             del self.cache_data[rm_key]
             print("DISCARD: {}".format(rm_key))
-
+        
         self.cache_data[key] = item
-
+    
     def get(self, key):
-        """get specific value"""
+        """return specific value"""
         if key is None:
             return None
-
+        
         return self.cache_data.get(key)
+
+
