698e754379a1f63206918df53bffb69a14d21f44
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-31 19:50:14 +0200

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""import libraries"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
+    def put(self, key, item):
+        """put key, value pair in dict"""
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get specific value"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
