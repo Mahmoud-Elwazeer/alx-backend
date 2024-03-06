22cf334a41121ed4fa198bcf67c367a86915cfbd
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-07 01:31:25 +0200

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import { createClient } from 'redis'
+
+
+// Create a Redis client
+const client = createClient();
+
+// Handling errors
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server')
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.quit()
+  }
+})
